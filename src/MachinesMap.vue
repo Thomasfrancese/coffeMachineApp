6050d930e37d8d63a106a3e15256e92ee28f79a2
Thomas <thomas.francese@campus-numerique-in-the-alps.com>
2018-03-08 16:58:14 +0100

    add markers map

--- src/MachinesMap.vue
+++ src/MachinesMap.vue
@@ -7,12 +7,28 @@
       :zoom="7"
       style="width: 100%; height: 800px"
     >
+      <gmap-marker v-for="machine in machines"
+                   v-bind:key="machine.id"
+                   v-bind:position="machine.position">
+ </gmap-marker>
     </gmap-map>
   </div>
 </template>
 
 <script>
-    export default {
-        name: 'mapping',
-    };
+  export default {
+    name: 'mapping',
+    data() {
+      return {
+        machines: [{
+          id: 1,
+          position:{lat:10, lng:10},
+        },
+          {
+            id: 2,
+            position:{lat:12, lng:9},
+          }]
+      };
+    }
+  };
 </script>
